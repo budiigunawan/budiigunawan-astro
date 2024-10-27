71123fe7f4e8639613797c1b2c35ca4d4228de39
Budi Gunawan <budiindra.g@gmail.com>
2024-10-27 12:06:05 +0700

    feat: hide blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,132 +1,129 @@
-import type { Site, Page, Links, Socials, Stacks } from "@types";
+import type { Site, Page, Links, Socials, Stacks } from '@types';
 
 // Global
 export const SITE: Site = {
-  TITLE: "Budi Indra Gunawan",
+  TITLE: 'Budi Indra Gunawan',
   DESCRIPTION:
-    "Welcome to my presonal website. A place to show my profile, projects, and blog.",
-  AUTHOR: "Budi I. Gunawan",
+    'Welcome to my presonal website. A place to show my profile, projects, and blog.',
+  AUTHOR: 'Budi I. Gunawan',
 };
 
 // Work Page
 export const WORK: Page = {
-  TITLE: "Work",
-  DESCRIPTION: "Places I have worked.",
+  TITLE: 'Work',
+  DESCRIPTION: 'Places I have worked.',
 };
 
 // Blog Page
 export const BLOG: Page = {
-  TITLE: "Blog",
-  DESCRIPTION: "Writing on topics I am passionate about.",
+  TITLE: 'Blog',
+  DESCRIPTION: 'Writing on topics I am passionate about.',
 };
 
 // Projects Page
 export const PROJECTS: Page = {
-  TITLE: "Projects",
-  DESCRIPTION: "Showcase of my works on web development",
+  TITLE: 'Projects',
+  DESCRIPTION: 'Showcase of my works on web development',
 };
 
 // Search Page
 export const SEARCH: Page = {
-  TITLE: "Search",
-  DESCRIPTION: "Search all posts and projects by keyword.",
+  TITLE: 'Search',
+  DESCRIPTION: 'Search all posts and projects by keyword.',
 };
 
 // Links
 export const LINKS: Links = [
   {
-    TEXT: "Home",
-    HREF: "/",
+    TEXT: 'Home',
+    HREF: '/',
   },
   {
-    TEXT: "Work",
-    HREF: "/work",
+    TEXT: 'Work',
+    HREF: '/work',
   },
   {
-    TEXT: "Projects",
-    HREF: "/projects",
+    TEXT: 'Projects',
+    HREF: '/projects',
   },
-  {
-    TEXT: "Blog",
-    HREF: "/blog",
-  },
+  // {
+  //   TEXT: 'Blog',
+  //   HREF: '/blog',
+  // },
 ];
 
 // Socials
 export const SOCIALS: Socials = [
   {
-    NAME: "Email",
-    ICON: "email",
-    TEXT: "budiindra.g@gmail.com",
-    HREF: "mailto:budiindra.g@gmail.com",
+    NAME: 'Email',
+    ICON: 'email',
+    TEXT: 'budiindra.g@gmail.com',
+    HREF: 'mailto:budiindra.g@gmail.com',
   },
   {
-    NAME: "LinkedIn",
-    ICON: "linkedin",
-    TEXT: "Budi Indra Gunawan",
-    HREF: "https://www.linkedin.com/in/budiigunawan/",
+    NAME: 'LinkedIn',
+    ICON: 'linkedin',
+    TEXT: 'Budi Indra Gunawan',
+    HREF: 'https://www.linkedin.com/in/budiigunawan/',
   },
   {
-    NAME: "Github",
-    ICON: "github",
-    TEXT: "budiigunawan",
-    HREF: "https://github.com/budiigunawan",
+    NAME: 'Github',
+    ICON: 'github',
+    TEXT: 'budiigunawan',
+    HREF: 'https://github.com/budiigunawan',
   },
 ];
 
 export const STACKS: Stacks = [
   {
-    TITLE: "HTML",
+    TITLE: 'HTML',
   },
   {
-    TITLE: "CSS",
+    TITLE: 'CSS',
   },
   {
-    TITLE: "Tailwind CSS",
+    TITLE: 'Tailwind CSS',
   },
   {
-    TITLE: "Javascript",
+    TITLE: 'Javascript',
   },
   {
-    TITLE: "Typescript",
+    TITLE: 'Typescript',
   },
   {
-    TITLE: "React JS",
+    TITLE: 'React JS',
   },
   {
-    TITLE: "Next JS",
+    TITLE: 'Next JS',
   },
   {
-    TITLE: "Vue",
+    TITLE: 'Vue',
   },
   {
-    TITLE: "Node JS",
+    TITLE: 'Node JS',
   },
   {
-    TITLE: "Express JS",
+    TITLE: 'Express JS',
   },
   {
-    TITLE: "Bun",
+    TITLE: 'PostgreSQL',
   },
   {
-    TITLE: "PostgreSQL",
+    TITLE: 'Prisma',
   },
   {
-    TITLE: "Prisma",
+    TITLE: 'Git',
   },
   {
-    TITLE: "Git",
+    TITLE: 'Jira',
   },
   {
-    TITLE: "Jira",
+    TITLE: 'Jest',
   },
   {
-    TITLE: "Jest",
+    TITLE: 'Cypress',
   },
   {
-    TITLE: "Cypress",
-  },
-  {
-    TITLE: "Figma",
+    TITLE: 'Figma',
   },
 ];
