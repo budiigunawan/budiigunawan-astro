19aa7ed728aa1044bf97ad7fb402ecb65554d885
Budi Gunawan <budiindra.g@gmail.com>
2024-11-03 20:01:43 +0700

    remove vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,10 +13,4 @@ export default defineConfig({
     solidJs(),
     tailwind({ applyBaseStyles: false }),
   ],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
